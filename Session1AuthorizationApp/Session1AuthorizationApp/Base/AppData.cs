8822db477c3e74daea048e6b0113ab0b5dad6b21
korkem <73012254+kkorkemm@users.noreply.github.com>
2021-12-01 15:11:42 +0600

    Session1

---
+++ Session1AuthorizationApp/Session1AuthorizationApp/Base/AppData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session1AuthorizationApp.Base
+{
+    /// <summary>
+    /// Работа с БД
+    /// </summary>
+    public class AppData
+    {
+        private static TelekomNevaSvyazDBEntities context;
+
+        /// <summary>
+        /// Получение контекста данных
+        /// </summary>
+        /// <returns> Модель данных </returns>
+        public static TelekomNevaSvyazDBEntities GetContext()
+        {
+            if (context == null)
+                context = new TelekomNevaSvyazDBEntities();
+
+            return context;
+        }
+
+        /// <summary>
+        /// Текущий сотрудник системы
+        /// </summary>
+        public static Employees CurrentEmployee { get; set; }
+    }
+}
---
+++ Session1AuthorizationApp/Session1AuthorizationApp/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Session1AuthorizationApp
+{
+    using Base;
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Employees Employee = new Employees();
+        private string CurrentCode = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Нажатие на кнопку Новый код
+        /// </summary>
+        private void BtnCode_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateRandomCode();
+        }
+
+        /// <summary>
+        /// Нажатие на кнопку Отмена
+        /// </summary>
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            TextLogin.Text = "";
+            TextPass.Password = "";
+            TextCode.Text = "";
+
+            TextPass.IsEnabled = false;
+            TextCode.IsEnabled = false;
+            BtnLogin.IsEnabled = false;
+        }
+
+        /// <summary>
+        /// Нажатие на кнопку Вход
+        /// </summary>
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentCode != TextCode.Text)
+            {
+                MessageBox.Show("Неверный код");
+                return;
+            }
+
+            AppData.CurrentEmployee = Employee;
+
+            MessageBox.Show($"{Employee.FullName}, Ваша роль: {Employee.EmployeeRoles.RoleName}", "Добро пожаловать!", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        /// <summary>
+        /// Нажатие Enter при вводе номер телефона
+        /// </summary>
+        private void TextLogin_KeyUp(object sender, KeyEventArgs e)
+        {
+            // проверка на то, что был нажат Enter
+            if (e.Key == Key.Enter)
+            {
+                string number = TextLogin.Text;
+                var user = AppData.GetContext().Employees.FirstOrDefault(p => p.TelNumber == number);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Сотрудника с таким номером нет в базе данных", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                else
+                {
+                    Employee = user;
+                    TextPass.IsEnabled = true;
+                    TextPass.Focus();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Нажатие Enter при вводе пароля
+        /// </summary>
+        private void TextPass_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string password = TextPass.Password;
+
+                if (Employee.Password == password)
+                {
+                    GenerateRandomCode();
+                    TextCode.IsEnabled = true;
+                    TextCode.Focus();
+                    BtnLogin.IsEnabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Неверный пароль", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Генерация случайного кода
+        /// </summary>
+        private void GenerateRandomCode()
+        {
+            string code = "";
+
+            string alphabet = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()-_=+";
+
+            Random random = new Random();
+
+            for (int i = 0; i < 8; i++)
+            {
+                int index = random.Next(0, alphabet.Length - 1);
+                code += alphabet[index];
+            }
+
+            CurrentCode = code;
+
+            MessageBox.Show(code, "Внимание! Ваш код!", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
